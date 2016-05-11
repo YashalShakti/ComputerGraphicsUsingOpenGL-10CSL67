ba796d7f1ac9d9f5d46fef0ee86d1ba0a1916427
Yashal Shakti <shaktiyashal@gmail.com>
2016-05-11 23:00:22 +0530

    Add a small reshape function

--- RotatingAHouse/RotatingAHouse.cpp
+++ RotatingAHouse/RotatingAHouse.cpp
@@ -1,60 +1,39 @@
 //
 // Created by yashal on 9/5/16.
+//Program to create a house like figure and rotate it about a given fixed point using OpenGL functions
 //
 
 #include "RotatingAHouse.h"
+typedef RotatingAHouse RH;
 
-GLfloat house[3][9] = {{100.0, 100.0, 175.0, 250.0, 250.0, 150.0, 150.0, 200.0, 200.0},
-                       {100.0, 300.0, 400.0, 300.0, 100.0, 100.0, 150.0, 150.0, 100.0},
-                       {1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0}};
+GLfloat house[3][NUM_VERTICES] = {{100.0, 100.0, 250.0, 175, 100.0, 250.0, 250.0, 100.0, 150.0, 150.0, 200.0, 200.0},
+                                  {100.0, 300.0, 300.0, 400, 300.0, 300.0, 100.0, 100.0, 100.0, 150.0, 150.0, 100.0},
+                                  {1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0}};
 GLfloat theta;
 
-void RotatingAHouse::multiply(GLfloat a[3][3], GLfloat b[3][9], GLfloat result[3][9]) {
+void RH::multiply(GLfloat a[3][3], GLfloat b[3][NUM_VERTICES], GLfloat result[3][NUM_VERTICES]) {
   for (int i = 0; i < 3; i++)
-    for (int j = 0; j < 9; j++) {
+    for (int j = 0; j < NUM_VERTICES; j++) {
       for (int k = 0; k < 3; k++)
         result[i][j] = result[i][j] + a[i][k] * b[k][j];
     }
 }
 
-void RotatingAHouse::drawHouse(GLfloat house[3][9]) {
-
-  int i;
-
-  //box
+void RH::drawHouse(GLfloat house[3][NUM_VERTICES]) {
   glColor3f(0.0, 0.0, 1.0);
   glBegin(GL_LINE_LOOP);
-  for (i = 0; i <= 4; i++) {
-    if (i != 2) {
-      glVertex2f(house[0][i], house[1][i]);
-    }
-  }
-  glEnd();
-
-  //door
-  glColor3f(1.0, 0.0, 0.0);
-  glBegin(GL_LINE_LOOP);
-  for (i = 5; i <= 8; i++) {
+  for (int i = 0; i < NUM_VERTICES; i++) {
     glVertex2f(house[0][i], house[1][i]);
   }
   glEnd();
-
-  //triangle
-  glColor3f(0.0, 0.0, 1.0);
-  glBegin(GL_LINE_LOOP);
-  for (i = 1; i <= 3; i++) {
-    glVertex2f(house[0][i], house[1][i]);
-  }
-  glEnd();
-
 }
-void RotatingAHouse::rotate(GLfloat theta, GLfloat result[3][9]) {
 
-  GLfloat m, n;
-  GLfloat h = 100.0;
-  GLfloat k = 100.0;
-  m = (GLfloat) (-h * (cos(theta) - 1) + k * (sin(theta)));// of form x-xcos(theta)+ysin(theta)
-  n = (GLfloat) (-k * (cos(theta) - 1) - h * (sin(theta)));// of form y-ycos(theta)-xsin(theta)
+void RH::rotate(GLfloat theta, GLfloat result[3][NUM_VERTICES]) {
+  double m, n;
+  GLfloat x = house[0][0];
+  GLfloat y = house[1][0];
+  m = x - (x * cos(theta) - y * sin(theta)); // of form x-xcos(theta)+ysin(theta)
+  n = y - (x * sin(theta) + y * cos(theta)); // of form y-ycos(theta)-xsin(theta)
 
   GLfloat rot_mat[3][3] = {
       {(GLfloat) cos(theta), (GLfloat) -sin(theta), m},
@@ -66,27 +45,31 @@ void RotatingAHouse::rotate(GLfloat theta, GLfloat result[3][9]) {
       {sin(theta),  cos(theta)  , n},
       {0         ,  0           , 1}
   };*/
-
   multiply(rot_mat, house, result);
 }
 
-void RotatingAHouse::display() {
-  GLfloat result[3][9] = {{0}, {0}, {0}};
+void RH::display() {
+  GLfloat result[3][NUM_VERTICES] = {{0}, {0}, {0}};
   drawHouse(house);//draw before rotation
   rotate(theta, result);//function to rotate
   drawHouse(result);
   glFlush();
 }
 
-void RotatingAHouse::initGl() {
+void RH::reshape(int w, int h) {
+  glViewport(0, 0, w, h);
+  initGl(w, h);
+}
+
+void RH::initGl(int w, int h) {
   glClearColor(1.0, 1.0, 1.0, 1.0);
   glClear(GL_COLOR_BUFFER_BIT);
   glMatrixMode(GL_PROJECTION);
   glLoadIdentity();
-  gluOrtho2D(0.0, 720.0, 0.0, 720.0);
+  glOrtho(0.0, w, 0.0, h, -1, 1);
 }
 
-int RotatingAHouse::main(int argc, char *argv[]) {
+int RH::main(int argc, char *argv[]) {
   std::cout << "Enter the rotation angle\n";
   std::cin >> theta;
   theta = theta * (3.14 / 180);
@@ -94,6 +77,7 @@ int RotatingAHouse::main(int argc, char *argv[]) {
   glutInitWindowSize(720, 720);
   glutCreateWindow("house rotation");
   glutDisplayFunc(display);
-  initGl();
+  glutReshapeFunc(reshape);
+  initGl(720, 720);
   glutMainLoop();
 }
--- RotatingAHouse/RotatingAHouse.h
+++ RotatingAHouse/RotatingAHouse.h
@@ -1,5 +1,5 @@
 //
-// Created by yashal on 9/5/16.
+// Created by yashal on NUM_VERTICES/5/16.
 //
 
 #ifndef CG2016_ROTATINGAHOUSE_H
@@ -9,16 +9,18 @@
 #include<math.h>
 #include<GL/glut.h>
 #include <iostream>
+const int NUM_VERTICES = 12;
 
 class RotatingAHouse {
  public:
   int main(int argc, char *argv[]);
  private:
-  void initGl();
+  static void initGl(int w, int h);
   static void display();
-  static void rotate(GLfloat theta, GLfloat result[3][9]);
-  static void multiply(GLfloat a[3][3], GLfloat b[3][9], GLfloat result[3][9]);
-  static void drawHouse(GLfloat house[3][9]);
+  static void rotate(GLfloat theta, GLfloat result[3][NUM_VERTICES]);
+  static void multiply(GLfloat a[3][3], GLfloat b[3][NUM_VERTICES], GLfloat result[3][NUM_VERTICES]);
+  static void drawHouse(GLfloat house[3][NUM_VERTICES]);
+  static void reshape(int w,int h);
 };
 
 
