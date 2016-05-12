89bbdee0304bba1f74feced24cdd969fec9b9222
Yashal Shakti <shaktiyashal@gmail.com>
2016-05-12 23:01:21 +0530

    Add Cstyle programs for simplicity

---
+++ CStyle/1.cpp
@@ -0,0 +1,81 @@
+//
+// Created by yashal on 10/5/16.
+// Program to recursively subdivide a tetrahedron to from 3D Sierpinski gasket. The
+// number of recursive steps is to be specified by the user
+//
+
+#include "GL/glut.h"
+
+typedef float Vertex[3];
+// Vertices of a tetrahedron in 3D viewed with one vertex pointing directly towards viewer.
+Vertex vertices[] = {{0.0, 0.0, 1.0f}, {0.0, 0.8f, 0.0f}, {-0.8f, -0.4f, 0.0f}, {0.8f, -0.4f, 0.0f}};
+
+void drawTriangle(Vertex a, Vertex b, Vertex c) {
+  glBegin(GL_POLYGON);
+  glVertex3fv(a);
+  glVertex3fv(b);
+  glVertex3fv(c);
+  glEnd();
+}
+
+void divideTriangle(Vertex a, Vertex b, Vertex c, int divisions) {
+  Vertex v1, v2, v3;
+  if (divisions > 0) { // If we still need to divide
+    for (int j = 0; j < 3; j++) {
+      v1[j] = (a[j] + b[j]) / 2;
+      v2[j] = (c[j] + a[j]) / 2;
+      v3[j] = (b[j] + c[j]) / 2;
+    }
+    // Divide triangle from a/b/c to two other midpoints they are connected to. (a is connected to in v1 and v2)
+    divideTriangle(a, v1, v2, divisions - 1);
+    divideTriangle(b, v3, v1, divisions - 1);
+    divideTriangle(c, v2, v3, divisions - 1);
+  } else {
+    drawTriangle(a, b, c);
+  }
+}
+
+void drawTetrahedron(int divisions) {
+  // Take each triangular face of Tetrahedron at a time and divide/draw it with different color.
+  glColor3f(1, 0, 0);
+  divideTriangle(vertices[0], vertices[1], vertices[2], divisions);
+  glColor3f(0, 0, 1);
+  divideTriangle(vertices[0], vertices[1], vertices[3], divisions);
+  glColor3f(0, 0, 0);
+  divideTriangle(vertices[0], vertices[2], vertices[3], divisions);
+  glColor3f(0, 1, 0);
+  divideTriangle(vertices[1], vertices[2], vertices[3], divisions);
+}
+
+void display() {
+  int divisions = 1; // Take input if need be.
+  drawTetrahedron(divisions);
+  glFlush();
+}
+
+void glInit(int w, int h) {
+
+  glClearColor(1, 1, 1, 1); // Set buffer color to White and clear using buffer.
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+  glEnable(GL_DEPTH_TEST);   // Enable hidden surface removal
+
+  glMatrixMode(GL_PROJECTION);   // Load identity matrix into Projection matrix
+  glLoadIdentity();
+
+  glViewport(0, 0, w, h); // Resize viewport to show new w and h.
+  if (h>=w) //This is needed to main proper aspect ratio; otherwise the image would appear stretched
+    glOrtho(-1.0, 1.0, (GLfloat) -h / w, (GLfloat) h / w, -1.0, 1.0); //width is smaller than height
+  else
+    glOrtho((GLfloat) -w / h, (GLfloat) w / h, -1.0, 1.0, -1.0, 1.0); //height is smaller
+}
+
+int main(int argc, char **argv) {
+  glutInit(&argc, argv);
+  glutInitWindowSize(720, 720);
+  glutCreateWindow("Sierpinski Gasket");
+  glutDisplayFunc(display);
+  glutReshapeFunc(glInit);
+  glInit(720,720);
+  glutMainLoop();
+}
---
+++ CStyle/2.cpp
@@ -0,0 +1,147 @@
+//
+// Created by yashal on 10/5/16.
+//
+#include "GL/glut.h"
+
+GLfloat   x0 = 0, y0 = 0, x1 = 120, y1 = 150, // small line
+          xMin = 20, yMin = 20, xMax = 100, yMax = 100, // windowport or small square
+          xvMin = 200, yvMin = 200, xvMax = 680, yvMax = 680; // viewport or big square
+
+float t0 = 0.0, t1 = 1.0;
+
+void drawLine(GLfloat x0, GLfloat y0, GLfloat x1, GLfloat y1) {
+  glBegin(GL_LINE_LOOP);
+  {
+    glVertex2f(x0, y0);
+    glVertex2f(x1, y1);
+  }
+  glEnd();
+}
+
+void drawSquare(GLfloat x0, GLfloat y0, GLfloat x1, GLfloat y1) {
+  glBegin(GL_LINE_LOOP);
+  {
+    glVertex2f(x0, y0);
+    glVertex2f(x1, y0);
+    glVertex2f(x1, y1);
+    glVertex2f(x0, y1);
+  }
+  glEnd();
+}
+
+void drawResult() {
+  drawSquare(xvMin, yvMin, xvMax, yvMax);
+  double sx, sy, vx0, vy0, vx1, vy1;
+  sx = (xvMax - xvMin) / (xMax - xMin);
+  sy = (yvMax - yvMin) / (yMax - yMin);
+  vx0 = xvMin + (x0 - xMin) * sx;
+  vy0 = yvMin + (y0 - yMin) * sy;
+  vx1 = xvMin + (x1 - xMin) * sx;
+  vy1 = yvMin + (y1 - yMin) * sy;
+  glColor3f(1.0, 0.0, 0.0);
+  drawLine(vx0, vy0, vx1, vy1);
+}
+
+int clipTest(float p, float q) {
+  float t = q / p;
+  // Aim is to increase t0 value and decrease t1 value to clip the line
+  // If p is negative; check for lower/minimum clipping boundary and vice-versa.
+  if (p < 0.0) {
+    if (t > t0) t0 = t; // If current t is > t0; update t0 value.
+    if (t > t1) return (0);
+  } else if (p > 0.0) {
+    if (t < t1) t1 = t; // If current t is < t1; update t1 value.
+    if (t < t0) return 0;
+  } else if (t == 0) {
+    if (q < 0.0) {
+      return 0;
+    }
+  }
+  return 1;
+}
+
+void clipAndDraw() {
+  float dx = x1 - x0, dy = y1 - y0;
+
+  if (clipTest(-dx, x0 - xMin)
+      && clipTest(dx, xMax - x0)
+      && clipTest(-dy, y0 - yMin)
+      && clipTest(dy, yMax - y0)) {
+
+    if (t1 < 1.0) {
+      x1 = x0 + t1 * dx;
+      y1 = y0 + t1 * dy;
+    }
+    if (t0 > 0.0) {
+      x0 = x0 + t0 * dx;
+      y0 = y0 + t0 * dy;
+    }
+
+    drawResult();
+  }
+}
+
+void display() {
+
+  glColor3i(1, 0, 0);
+  drawLine(x0, y0, x1, y1);
+
+  glColor3i(0, 0, 1);
+  drawSquare(xMin, yMin, xMax, yMax);
+
+  clipAndDraw();
+  glFlush();
+}
+
+void glInit() {
+  glClearColor(1, 1, 1, 1);
+  glClear(GL_COLOR_BUFFER_BIT);
+
+  /*glMatrixMode(GL_PROJECTION);
+  glLoadIdentity();*/
+
+  glOrtho(0, 720, 0, 720, -1, 1);
+}
+
+int main(int argc, char **argv) {
+  // Get/store vertices of the original line, windowport and viewport using any method of choice.
+  //getValues();  Not really needed unless someone asks
+  glutInit(&argc, argv);
+  glutInitWindowSize(720, 720);
+  glutCreateWindow("Liang Barsky line clipping");
+  glInit();
+  glutDisplayFunc(display);
+  glutMainLoop();
+}
+
+/*
+ * Not really needed unless someone asks
+ *
+ * void getValues() {
+  using namespace std;
+  cout << "Do you want to input values? y/n:";
+  char choice;
+  cin >> choice;
+  if (choice == 'n') {
+    cout << "Using default values";
+    return;
+  }
+  cout << "Enter x0,y0 and x1,y1\n";
+  cin >> x0;
+  cin >> y0;
+  cin >> x1;
+  cin >> y1;
+
+  cout << "Enter xMin,yMin and xMax,yMax\n";
+  cin >> xMin;
+  cin >> yMin;
+  cin >> xMax;
+  cin >> yMax;
+
+  cout << "Enter xvMin,yvMin and xvMax,yvMax\n";
+  cin >> xvMin;
+  cin >> yvMin;
+  cin >> xvMax;
+  cin >> yvMax;
+}
+*/
